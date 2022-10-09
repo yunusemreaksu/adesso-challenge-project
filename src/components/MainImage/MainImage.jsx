0870eec1303df41cc9a4871d62a758d25a6ad04c
Yunus Emre Aksu <emre_aksu@live.com>
2022-10-09 11:23:25 +0300

    Code refactoring

--- src/components/MainImage/MainImage.jsx
+++ src/components/MainImage/MainImage.jsx
@@ -1,8 +1,10 @@
-import aoe_image from "../../assets/AoE.jpg";
+import aoeImageUrl from "../../assets/AoE.jpg";
 import classes from "./MainImage.module.scss";
 
 const MainImage = () => {
-  return <img src={aoe_image} alt="Age of Empires" className={classes.image} />;
+  return (
+    <img src={aoeImageUrl} alt="Age of Empires" className={classes.image} />
+  );
 };
 
 export default MainImage;
